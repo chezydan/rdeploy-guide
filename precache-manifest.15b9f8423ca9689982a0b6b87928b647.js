6163ec19058d971d71b6ecbced10545ade3bb3f2
dan <chezydan@gmail.com>
2020-10-07 19:59:35 +0300

    Updates

---
+++ precache-manifest.15b9f8423ca9689982a0b6b87928b647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc604c6fe822c31d0ea605c55c7f11a",
+    "url": "/rdeploy-guide/index.html"
+  },
+  {
+    "revision": "376052eec65d0ae637a8",
+    "url": "/rdeploy-guide/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b4105e3108a73bcfe10",
+    "url": "/rdeploy-guide/static/js/2.ad591e98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rdeploy-guide/static/js/2.ad591e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376052eec65d0ae637a8",
+    "url": "/rdeploy-guide/static/js/main.dca25781.chunk.js"
+  },
+  {
+    "revision": "f9645c4a4c05d52fc5ba",
+    "url": "/rdeploy-guide/static/js/runtime-main.7571c59c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rdeploy-guide/static/media/logo.5d5d9eef.svg"
+  }
+]);
